3905ea72cc9893c753b792e72076298158c0e148
Debban-art <debbany.545@gmail.com>
2024-12-04 09:50:48 -0600

    Formulario OrdenCompra

--- src/app/modules/pages/ordenes-compras/ordenes-compras/ordenes-compras.component.ts
+++ src/app/modules/pages/ordenes-compras/ordenes-compras/ordenes-compras.component.ts
@@ -1,19 +1,35 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { NgFor, NgIf } from '@angular/common';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { NbCardModule, NbSelectModule, NbButtonModule, NbDatepickerModule, NbInputModule } from '@nebular/theme';
 import { OrdenCompraInsertRequest } from '@Models/OrdenCompra';
-import { OrdenCompraService } from '@Services';
+import { OrdenCompraService, SucursalService } from '@Services';
+import { ProveedorModel } from '@Models/Proveedor';
+import { ProveedorService } from '@Services';
+import { SucursalModel } from '@Models/Sucursal';
 
 @Component({
   selector: 'app-ordenes-compras',
   standalone: true,
-  imports: [ReactiveFormsModule, NbCardModule, NbSelectModule, NbButtonModule, NbDatepickerModule, NbInputModule],
+  imports: [ReactiveFormsModule, NgFor, NgIf,NbCardModule, NbSelectModule, NbButtonModule, NbDatepickerModule, NbInputModule],
   templateUrl: './ordenes-compras.component.html',
   styleUrls: ['./ordenes-compras.component.scss']
 })
-export class OrdenesComprasComponent {
+export class OrdenesComprasComponent implements OnInit{
   private ordenCompraService = inject(OrdenCompraService);
+  private proveedorService = inject(ProveedorService);
+  private sucursalService = inject(SucursalService);
   private fb = inject(FormBuilder)
+
+  proveedoresList: ProveedorModel[] = [];
+  sucursalesList: SucursalModel[] = [];
+  compradoresList = [
+    {id: 1, nombre:"BSaravia"},
+    {id: 2, nomnre:"RSillas"},
+    {id: 3, nombre:"JPerez"},
+    {id: 8, nombre:"GBorrego"},
+    {id:16, nombre:"SVillareal"}
+  ]
 
   form = this.fb.nonNullable.group({
     id: [0],
@@ -22,6 +38,23 @@ export class OrdenesComprasComponent {
     idSucursal: [0, [Validators.required, Validators.min(1)]],
     idComprador: [0, [Validators.required, Validators.min(1)]],
   })
+
+  ngOnInit(): void {
+    this.getProveedores();
+    this.getSucursales();
+  }
+
+  getProveedores(): void {
+    this.proveedorService.getProveedores().subscribe((data) =>{
+      this.proveedoresList = data.Response.data;
+    })
+  }
+
+  getSucursales(): void {
+    this.sucursalService.getSucursales().subscribe((data) => {
+      this.sucursalesList = data;
+    })
+  }
 
   onSubmit(): void {
     if (this.form.valid)
