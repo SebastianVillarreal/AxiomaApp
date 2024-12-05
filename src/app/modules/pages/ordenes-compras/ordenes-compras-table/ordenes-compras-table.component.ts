f42af1c0a56a537cf1b7cc7f746ff29386a7319b
Debban-art <debbany.545@gmail.com>
2024-12-04 23:43:07 -0600

    Componente Orden Compra Table

---
+++ src/app/modules/pages/ordenes-compras/ordenes-compras-table/ordenes-compras-table.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ordenes-compras-table',
+  templateUrl: './ordenes-compras-table.component.html',
+  styleUrls: ['./ordenes-compras-table.component.scss']
+})
+export class OrdenesComprasTableComponent {
+
+}
